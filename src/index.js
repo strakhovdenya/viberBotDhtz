209025db6466e617d86ebc7bdb353fedf65c7e18
Your Name <you@example.com>
2021-02-27 14:44:48 +0200

    ignore

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const BotEvents = require('viber-bot').Events;
 const TextMessage = require('viber-bot').Message.Text;
 const PictureMessage = require('viber-bot').Message.Picture;
 const RichMediaMessage = require('viber-bot').Message.RichMedia;
+var logger = require("./logger.js");
+
 require('dotenv').config();
 
 const winston = require('winston');
@@ -13,14 +15,7 @@ const ngrok = require('./get_public_url');
 
 var request = require('request');
 
-function createLogger() {
-    const logger = new winston.Logger({
-        level: "debug" // We recommend using the debug level for development
-    });
 
-    logger.add(winston.transports.Console, toYAML.config());
-    return logger;
-}
 
 function say(response, message) {
     response.send(new TextMessage(message));
@@ -56,7 +51,7 @@ function say(response, message) {
 //     })
 // }
 
-const logger = createLogger();
+
 
 
 // Creating the bot with access token, name and avatar
@@ -66,18 +61,18 @@ const bot = new ViberBot(logger, {
     avatar: "https://raw.githubusercontent.com/devrelv/drop/master/151-icon.png" // Just a placeholder avatar to display the user
 });
 
-bot.onError(err => logger.error(err));
+bot.onError(err => logger.log(err));
 
-bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) => {
-        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-        onFinish(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`))
-    }
-);
 
 // The user will get those messages on first registration
 bot.onSubscribe(response => {
     say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
 });
+
+bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) => {
+        onFinish(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`))
+    }
+);
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     // This sample bot can answer only text messages, let's make sure the user is aware of that.
@@ -123,16 +118,12 @@ bot.onTextMessage(/./, (message, response) => {
     if (message.text === 'shedule_month') {
         response.send([
                 new PictureMessage('https://res.cloudinary.com/hxrdi6ylu/image/upload/v1614417847/dhtz/shedule_feb_kfncaw.jpg', null, 'https://res.cloudinary.com/hxrdi6ylu/image/upload/v1614417847/dhtz/shedule_feb_kfncaw.jpg'),
-                new TextMessage(`${response.userProfile.name} лови рассписание на месяц`, OPTION_KEYBOARD),
+                new TextMessage(`${response.userProfile.name} \r\n лови рассписание на месяц`, OPTION_KEYBOARD),
             ]
         );
     } else {
         response.send(
             new TextMessage(`${response.userProfile.name} привет!!!!`, OPTION_KEYBOARD),
-
-            // new PictureMessage('https://git.heroku.com/viberhelperdhtz.git/src/images/hockey-logo-vector_20448-291.jpg', null, 'https://git.heroku.com/viberhelperdhtz.git/src/images/hockey-logo-vector_20448-291.jpg'),
-
-            // new RichMediaMessage(DAY_BUTTONS);
         );
     }
 
@@ -145,3 +136,31 @@ const port = process.env.PORT || 8080;
 
 http.createServer(bot.middleware()).listen(port, () => bot.setWebhook('https://viberhelperdhtz.herokuapp.com/'));
 
+// 01.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 02.02.2021	  	 	                            18.45 - 20.00	18.30
+// 03.02.2021	 	 	                            18.45 - 20.00	18.30
+// 04.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 05.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 06.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 07.02.2021	06.15 - 07.45	Салтівський лід	 	                06.00
+// 08.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 09.02.2021	 	 	                            18.45 - 20.00   18.30
+// 10.02.2021	 	 	                            18.45 - 20.00   18.30
+// 11.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 12.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 13.02.2021	20.30 - 21.30	Палац Спорту	   	                19.45
+// 14.02.2021	06.15 - 07.45	Салтівський лід	 	                06.00
+// 15.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 16.02.2021	  	 	                            18.45 - 20.00	18.30
+// 17.02.2021	 	 	                            18.45 - 20.00	18.30
+// 18.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 19.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 20.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 21.02.2021	06.15 - 07.45	Салтівський лід	 	                06.00
+// 22.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 23.02.2021	 	 	                            18.45 - 20.00	18.30
+// 24.02.2021	 	 	                            18.45 - 20.00	18.30
+// 25.02.2021	20.30 - 21.30	Палац Спорту	 	                19.45
+// 26.02.2021	21.00 - 22.00	Палац Спорту	 	                20.15
+// 27.02.2021	20.30 - 21.30	Палац Спорту	  	                19.45
+// 28.02.2021	06.15 - 07.45	Салтівський лід	   	                06.00
