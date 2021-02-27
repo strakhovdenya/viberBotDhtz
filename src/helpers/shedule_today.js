f74df7a5aa040070633fd7d2b19dffc01ccef57b
Your Name <you@example.com>
2021-02-27 17:28:53 +0200

    ignore

--- src/helpers/shedule_today.js
+++ src/helpers/shedule_today.js
@@ -6,11 +6,16 @@ const constants = require("./constants.js");
 
 const SCHEDULE_URL = 'https://res.cloudinary.com/hxrdi6ylu/image/upload/v1614417847/dhtz/shedule_feb_kfncaw.jpg';
 
-exports.send = function(response){
+exports.send = function (response) {
     const date = new Date();
-    const month = date.getMonth()+1;
+
+    const day = date.getDate();
+    const dayForatted = (day < 10 ? '0' : '') + day;
+
+    const month = date.getMonth() + 1;
     const monthForatted = (month < 10 ? '0' : '') + month;
-    const formattedDate = date.getDate() + '.' + monthForatted + '.' + date.getFullYear();
+
+    const formattedDate = day + '.' + monthForatted + '.' + date.getFullYear();
     response.send([
             new TextMessage(`${response.userProfile.name} лови рассписание на ${formattedDate}`, constants.OPTION_KEYBOARD),
         ]
