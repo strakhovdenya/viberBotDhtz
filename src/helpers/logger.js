e35324a429c851d4bfe3369d68648c6581289383
Your Name <you@example.com>
2021-03-16 22:34:08 +0200

    start menu junior

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -6,7 +6,7 @@ const toYAML = require('winston-console-formatter');
 
 exports.createLogger = function () {
     const logger = new winston.Logger({
-        level: "error" // We recommend using the debug level for development
+        level: "debug" // We recommend using the debug level for development  error
     });
 
     logger.add(winston.transports.Console, toYAML.config());
