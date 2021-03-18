0a62ff210ca09cd3222df26c19837b8f53ebec47
Your Name <you@example.com>
2021-03-18 22:25:26 +0200

    shedule elder

--- src/helpers/shedule_month_elder.js
+++ src/helpers/shedule_month_elder.js
@@ -5,12 +5,12 @@ const PictureMessage = require('viber-bot').Message.Picture;
 const constants = require("./constants.js");
 const StickerMessage = require('viber-bot').Message.Sticker;
 
-const SCHEDULE_URL = 'https://res.cloudinary.com/hxrdi6ylu/image/upload/v1614541343/dhtz/mart_2021_phru8m.png';
+const SCHEDULE_URL = 'https://res.cloudinary.com/hxrdi6ylu/image/upload/v1616099041/dhtz/mart_elder_2021_lj7uzc.png';
 
 exports.send = function(response){
     response.send([
-            // new PictureMessage(SCHEDULE_URL, null, SCHEDULE_URL),
-            new StickerMessage(40133),
+            new PictureMessage(SCHEDULE_URL, null, SCHEDULE_URL),
+            // new StickerMessage(40133),
             new TextMessage(`${response.userProfile.name} лови рассписание на месяц для старших`, constants.OPTION_KEYBOARD_ELDER),
         ]
     );
