b0f724c4f39cf2cccfca25357823f5c9aac742bd
Your Name <you@example.com>
2021-05-02 12:21:25 +0300

    add connection to db

--- src/helpers/shedule_today_junior.js
+++ src/helpers/shedule_today_junior.js
@@ -3,19 +3,16 @@ var exports = module.exports = {};
 const sheduleDayAnswersJunior = require("./sheduleDayAnswersJunior.js");
 const constants = require("../constants/sheduleJunior.js");
 const dateFormatter = require("./dateFormatter.js");
-const mongoDate = require("./mongoDateFormatter.js");
-const currentMonth = require("./currentMonthYearFormatter.js");
 const Schedule = require('../models/schedule');
 exports.send = async function (response) {
     const formattedDate = dateFormatter();
-    const currentMongoDate = mongoDate();
 
     const scheduleDay = await Schedule.find({data: {$eq: formattedDate}});
-    console.log(currentMongoDate);
-    console.log(formattedDate);
-    console.log(scheduleDay);
 
-    const objDay = constants.SHEDULE_JUNIOR[formattedDate];
+    let [objDay] =  scheduleDay;
+    console.log(objDay);
+
+     objDay = constants.SHEDULE_JUNIOR[formattedDate];
 
     let ansver;
 
