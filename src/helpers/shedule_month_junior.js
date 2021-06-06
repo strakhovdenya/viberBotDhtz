d1d86062b4a670c68ac93ef568ecd11d60a6044c
Your Name <you@example.com>
2021-06-06 07:25:21 +0300

    add stub for camp schedule

--- src/helpers/shedule_month_junior.js
+++ src/helpers/shedule_month_junior.js
@@ -8,8 +8,9 @@ const SCHEDULE_URL = 'https://res.cloudinary.com/hxrdi6ylu/image/upload/a_-90/v1
 
 exports.send = function(response){
     response.send([
-            new PictureMessage(SCHEDULE_URL, null, SCHEDULE_URL),
-            new TextMessage(`${response.userProfile.name} лови расписание сборов для младших`, constants.OPTION_KEYBOARD_JUNIOR),
+            // new PictureMessage(SCHEDULE_URL, null, SCHEDULE_URL),
+            // new TextMessage(`${response.userProfile.name} лови расписание сборов для младших`, constants.OPTION_KEYBOARD_JUNIOR),
+            new TextMessage(`${response.userProfile.name} расписание сборов для младших уточняятся`, constants.OPTION_KEYBOARD_JUNIOR),
         ]
     );
 }
