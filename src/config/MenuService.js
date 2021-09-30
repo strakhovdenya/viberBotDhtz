92a00c0f5d8139085b8180ee5d95370eea6b003d
Your Name <you@example.com>
2021-09-30 16:04:55 +0300

    fix

--- src/config/MenuService.js
+++ src/config/MenuService.js
@@ -3,7 +3,7 @@ import {MenuModel} from "../models/menu.js";
 export class MenuService {
     static async getInstance() {
         if (!MenuService.instance) {
-            MenuService.instance = await MenuModel.find({$eq:{level:"start"}});
+            MenuService.instance = await MenuModel.find({level: {$eq: "start"}});
             console.log('get menu');
         }
         console.log('return menu');
