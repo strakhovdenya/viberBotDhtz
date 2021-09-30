570a5bce390762e28ca209368196558240f2f547
Your Name <you@example.com>
2021-09-30 16:38:22 +0300

    menu

--- src/config/MenuService.js
+++ src/config/MenuService.js
@@ -4,15 +4,13 @@ export class MenuService {
     static async getInstance(levelForFind) {
         if (!MenuService.instance) {
             MenuService.instance = new Map();
-            console.log('init  Map');
-            // MenuService.instance = await MenuModel.findOne({level: {$eq: levelForFind}});
-            // console.log('get menu');
+            // console.log('init  Map');
         }
         if(MenuService.instance.get(levelForFind)=== undefined){
-            console.log('set map');
+            // console.log('set map');
             MenuService.instance.set(levelForFind, await MenuModel.findOne({level: {$eq: levelForFind}}));
         }
-        console.log('return menu');
+        // console.log('return menu');
         return MenuService.instance.get(levelForFind);
     }
 }
