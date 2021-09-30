ddaa9b06302099a679c77e822d6f52ff471372ab
Your Name <you@example.com>
2021-09-30 16:26:13 +0300

    menu

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -63,7 +63,7 @@ bot.onTextMessage(/./, async (message, response) => {
         process.env.MSG_TOKEN = message.token;
     }
 
-    const [startMenu] = await MenuService.getInstance();
+    const startMenu = await MenuService.getInstance();
     console.log(startMenu);
     if (message.text === 'shedule_month_junior') {
         sheduleMonthJunior(response);
@@ -82,9 +82,11 @@ bot.onTextMessage(/./, async (message, response) => {
     } else if (message.text === 'elder') {
         response.send(new Bot.Message.Text(`${response.userProfile.name} вот меню для старших`, constants.OPTION_KEYBOARD_ELDER));
     } else if (message.text === 'start') {
+        const startMenu = await MenuService.getInstance('start');
         response.send(new Bot.Message.Text(`${response.userProfile.name} привет!!!!`, startMenu));
     } else {
-        response.send(new Bot.Message.Text(`${response.userProfile.name} привет!!!!`, constants.OPTION_KEYBOARD_START));
+        const startMenu = await MenuService.getInstance('start');
+        response.send(new Bot.Message.Text(`${response.userProfile.name} привет!!!!`, startMenu));
     }
 });
 
--- src/config/MenuService.js
+++ src/config/MenuService.js
@@ -1,9 +1,9 @@
 import {MenuModel} from "../models/menu.js";
 
 export class MenuService {
-    static async getInstance() {
+    static async getInstance(levelForFind) {
         if (!MenuService.instance) {
-            MenuService.instance = await MenuModel.find({level: {$eq: "start"}});
+            MenuService.instance = await MenuModel.findOne({level: {$eq: levelForFind}});
             console.log('get menu');
         }
         console.log('return menu');
