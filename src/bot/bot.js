79079e109d0eed4b1f1cdb424963d389b30a41ba
Your Name <you@example.com>
2021-10-01 11:17:07 +0300

    default answer

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -10,6 +10,7 @@ import {scheduleTomorrowElder} from "../helpers/scheduleDaysAnswers.js";
 import {con as constants} from "../helpers/constants.js";
 import {} from 'dotenv/config.js';
 import {MenuService} from "../config/MenuService.js";
+import getDefaultAnswer from "../helpers/defaultAnswer/Answer";
 
 
 
@@ -77,7 +78,8 @@ bot.onTextMessage(/./, async (message, response) => {
     } else if (message.text === 'shedule_tomorrow_elder') {
         await scheduleTomorrowElder(response);
     } else if (message.text === 'junior') {
-        response.send(new Bot.Message.Text(`${response.userProfile.name} вот меню для младших`, constants.OPTION_KEYBOARD_JUNIOR));
+        // response.send(new Bot.Message.Text(`${response.userProfile.name} вот меню для младших`, constants.OPTION_KEYBOARD_JUNIOR));
+        response.send(await getDefaultAnswer(response.userProfile.name));
     } else if (message.text === 'elder') {
         response.send(new Bot.Message.Text(`${response.userProfile.name} вот меню для старших`, constants.OPTION_KEYBOARD_ELDER));
     } else if (message.text === 'start') {
---
+++ src/helpers/defaultAnswer/Answer.js
@@ -0,0 +1,17 @@
+import {AnswerModel} from "../../models/answer.js";
+import Bot from "viber-bot";
+
+
+export default async function getDefaultAnswer(userName) {
+    const levelData = await AnswerModel.findOne({level: {$eq: this.level}, isDefault: {$eq: true}});
+    const answerResponse = [];
+    for (let textType in levelData.answerText) {
+        if (textType.sticker) {
+            answerResponse.push(new Bot.Message.Sticker(textType.sticker))
+        }
+
+        if (textType.text) {
+            answerResponse.push(new Bot.Message.Text(userName + ' '+textType.text, levelData.menu))
+        }
+    }
+}
