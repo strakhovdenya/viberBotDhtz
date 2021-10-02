ea5aecba7f66f82293d4ca2c5604c8503b5e5e0b
Your Name <you@example.com>
2021-10-02 11:36:09 +0300

    add schema

--- src/models/scheduleElder.js
+++ src/models/scheduleElder.js
@@ -1,29 +1,6 @@
 import mongoose  from "mongoose";
 
-const Schema = mongoose.Schema;
-
-const ScheduleSchema = new Schema({
-    data: {
-        type: String,
-        require: true
-    },
-    year_month: {
-        type: String,
-        require: true
-    },
-    time_ice: {
-        type: String
-    },
-    ice_place: {
-        type: String
-    },
-    time_ground: {
-        type: String
-    },
-    gathering_time: {
-        type: String
-    }
-});
+import ScheduleSchema from "./schema/scheduleSchema.js";
 
 export const  ScheduleElder = mongoose.model('ScheduleElder', ScheduleSchema);
 
